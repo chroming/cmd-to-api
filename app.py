c9fd2d78d46910fb4525a16d3099391947e4ddc7
chroming <chroming@live.com>
2018-08-27 22:40:58 +0800

    init

---
+++ app.py
@@ -0,0 +1,50 @@
+
+import json
+import subprocess
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+CMDS = {
+    "ll": ["ls", "-al"],
+    "x": "whoami"
+}
+
+
+def run_cmd(command):
+    try:
+        output = subprocess.check_output(command)
+    except FileNotFoundError:
+        output = "COMMAND NOT FOUND !"
+    except subprocess.CalledProcessError as e:
+        output = e.output
+    except Exception as e:
+        output = e.args
+    return output
+
+
+def cmd_filters(cmd):
+    return CMDS[cmd] if cmd in CMDS else False
+
+
+def handle_cmd(cmd):
+    return run_cmd(CMDS.get(cmd))
+
+
+@app.route('/')
+def hello_world():
+    return json.dumps(CMDS)
+
+
+@app.route('/<cmd>')
+def api(cmd):
+    if cmd_filters(cmd):
+        return handle_cmd(cmd)
+    else:
+        return "NO CMD !"
+
+
+if __name__ == '__main__':
+    app.run()
